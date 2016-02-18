ce2e8ad7bf76c77b86516d9cb2f2fd42101ddaef
Baldur Bjarnason <bb@baldurbjarnason.com>
2016-02-18 15:45:04 +0000

    Fix for id and url bugs.

--- import/tagsoup.js
+++ import/tagsoup.js
@@ -9,12 +9,15 @@ var url = require("url");
 var resizeImagesToMatch = require("./images.js").resizeImagesToMatch;
 
 function wrapChapters ($, selectors) {
+  var counter = getZeroPaddedStringCounter();
   var chapters = [];
   $(selectors).each(function () {
-    var chapter = cheerio.load("<div></div>");
-    chapter.append(this);
-    chapter.append($(this).nextUntil(selectors));
-    chapters.push(chapter.xml());
+    var chapter = $("<div class='chapter-wrapper'></div>");
+    var chapterBody = $(this).nextUntil(selectors);
+    chapterBody.remove();
+    chapter.append($(this));
+    chapter.append(chapterBody);
+    chapters.push({ contents: chapter.html(), htmlId: "chapter" + counter() });
   });
   return chapters;
 }
@@ -40,6 +43,14 @@ function createBookFromTagsoup (filename, target, options) {
       normalizeWhitespace: true,
       xmlMode: options.xml
     });
+    if (options.counter) {
+      $("*").each(function () {
+        if (!$(this).attr("id")) {
+          var hash = "loc" + options.counter();
+          $(this).attr("id", hash);
+        }
+      });
+    }
     if (options.transforms) {
       for (var selector in options.transforms) {
         $(selector).each(options.transforms[selector]);
@@ -50,6 +61,11 @@ function createBookFromTagsoup (filename, target, options) {
     }
     if (!options.scripts) {
       $ = html.stripScripts($);
+    }
+    if (options.process) {
+      options.process.forEach(function (item) {
+        $(item.selector).each(item.processFunction($));
+      });
     }
     book.images = [];
     $("[src]").each(function () {
@@ -65,7 +81,9 @@ function createBookFromTagsoup (filename, target, options) {
         img.type = "image/gif";
       }
       img.target = target;
-      fs.copySync(path.resolve(filename, $(this).attr("src")), path.resolve(target, filename, $(this).attr("src")));
+      var srcFile = url.resolve(filename, $(this).attr("src"));
+      var targetFile = url.resolve(target, srcFile);
+      fs.copySync(path.resolve(srcFile), path.resolve(targetFile));
       if (options.forceImgType !== false) {
         $(this).attr("src", src.replace(path.extname(src), "." + options.forceImgType));
         img.newhref = $(this).attr("src");
